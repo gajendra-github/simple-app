6c2cb7c3adc29e4d2bb8db6638258262902170c9
gajendra-github <gajendra.github@gmail.com>
2023-03-05 12:00:44 -0500

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '/views/')));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 
 
