40fc3a3765989358afd3bf9acdea42fc49533ea5
gajendra-github <gajendra.github@gmail.com>
2023-03-05 11:49:38 -0500

    update

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, '/views/')));
+
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    //res.render('index', { title: 'Welcome to ', data: ['1', '2', '3'] });
+    res.send("Building App with Node Express Mongo Heroku");
 });
 
 app.listen(port, () => {
